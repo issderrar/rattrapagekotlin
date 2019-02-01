d230f69a019896c1854038641eb7ead832b61eae
Issam <issam_derrar@yahoo.fr>
2019-02-01 14:15:25 +0100

    another commit

--- src/main/kotlin/Device.kt
+++ src/main/kotlin/Device.kt
@@ -1,36 +1,15 @@
-class Device {
-    var year: Int = 0
-    var device: String? = null
-    var commande: Int = 0
-    var impressions: Int = 0
-    var clics: Int = 0
-    var cost: Int = 0
-    var pm: Int = 0
-    var ca: Int = 0
-    var month: String? = null
+class Device(
+    var year: Int,
+    var device: String?,
+    var commande: Int,
+    var impressions: Int,
+    var clics: Int,
+    var cost: Int,
+    var pm: Int,
+    var ca: Int,
+    var month: String?
+) {
 
-
-    constructor(
-        year: Int,
-        device: String?,
-        commande: Int,
-        impressions: Int,
-        clics: Int,
-        cost: Int,
-        pm: Int,
-        ca: Int,
-        month: String?
-    ) {
-        this.year = year
-        this.device = device
-        this.commande = commande
-        this.impressions = impressions
-        this.clics = clics
-        this.cost = cost
-        this.pm = pm
-        this.ca = ca
-        this.month = month
-    }
 
     override fun toString(): String {
         return "Appareil [year=$year, device=$device, commande=$commande, impressions=$impressions, clics=$clics, cost=$cost, pm=$pm, ca=$ca, month=$month]"
