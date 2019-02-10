cd4a2b8cd28cb3c51c698b62112d4560e275a6e4
Issam <issam_derrar@yahoo.fr>
2019-02-10 23:59:07 +0100

    fixed:(RoiPerDevicePerMonthPerCalculator was showing the Ca (bad copy/pasta)

--- src/main/kotlin/com/issderrar/kpi/ROIPerDevicePerMonthCalculator.kt
+++ src/main/kotlin/com/issderrar/kpi/ROIPerDevicePerMonthCalculator.kt
@@ -5,20 +5,20 @@ import com.jakewharton.fliptables.FlipTable
 
 class ROIPerDevicePerMonthCalculator : KpiStrategy {
     override fun computeKpi(devices: List<Device>) {
-        val turnoverGroupedByDevice = devices.groupBy { it.device }
-        turnoverGroupedByDevice.forEach { device, devices ->
+        val RoiGroupedByDevice = devices.groupBy { it.device }
+        RoiGroupedByDevice.forEach { device, devices ->
 
-            val turnoverGroupedByMonth = devices.groupBy { it.month }
+            val RoiGroupedByMonth = devices.groupBy { it.month }
 
             val headers = arrayOf("Mois", "ROI")
-            val count = turnoverGroupedByMonth.size
+            val count = RoiGroupedByMonth.size
             val data = Array(size = count) { Array(size = 2) { "" } } // String[count][2]
 
             var index = 0
-            turnoverGroupedByMonth.forEach { month, devicesMonth ->
+            RoiGroupedByMonth.forEach { month, devicesMonth ->
                 val row = data[index]
                 row[0] = "$month"
-                row[1] = "${devicesMonth.sumBy { it.ca }}"
+                row[1] = "${(devicesMonth.sumBy { it.ca })/(devicesMonth.sumBy{it.cost}.toDouble())}"
                 index++
             }
             val outerHeaders = arrayOf("Appareil", "CA par mois")
